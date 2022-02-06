061a82b539a976942d89cdaa349f6197ef4ff0b2
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-02-06 23:27:37 +0600

    add s3 file upload

--- src/climsoft_api/config.py
+++ src/climsoft_api/config.py
@@ -8,7 +8,11 @@ BASE_DIR: str = os.path.dirname(os.path.abspath(__file__))
 class Settings(BaseSettings):
     SECRET_KEY: str = "some-random-unique-secret-key"
     DATABASE_URI: AnyUrl = "mysql+mysqldb://root:password@mariadb:3306/climsoft"
-    FILE_STORAGE: str = "disk"
+    FILE_STORAGE: str = "s3"
+    S3_BUCKET: str = "climsoft-paper-archive"
+    AWS_REGION: str = "eu-west-2"
+    AWS_ACCESS_KEY_ID: str = "replace it"
+    AWS_SECRET_ACCESS_KEY: str = "replace it"
 
     class Config:
         env_prefix = "CLIMSOFT_"

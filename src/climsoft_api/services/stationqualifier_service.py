e8ccaa912fb8d93cfcebe9833fe085dca2e6d5a9
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-02-28 11:25:05 +0600

    fix name error

--- src/climsoft_api/services/stationqualifier_service.py
+++ src/climsoft_api/services/stationqualifier_service.py
@@ -6,6 +6,7 @@ from opencdms.models.climsoft import v4_1_1_core as models
 from climsoft_api.api.stationqualifier import schema as stationqualifier_schema
 from fastapi.exceptions import HTTPException
 from climsoft_api.utils.query import get_count
+from gettext import gettext as _
 
 logger = logging.getLogger("ClimsoftStationQualifierService")
 logging.basicConfig(level=logging.INFO)

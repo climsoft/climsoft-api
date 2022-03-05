9c3e5e40567c9ce045e3c3d79c3e68cec5b1aced
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-03-05 09:37:11 +0600

    raise error to check filepath

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import logging
+import os.path
+
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 from babel.messages.frontend import compile_catalog
@@ -5,10 +8,27 @@ from babel.messages.frontend import compile_catalog
 with open("requirements.txt", "r") as requirements_file:
     requirements = [r.strip("\n") for r in requirements_file.readlines()]
 
+logger = logging.getLogger("SetupLogger")
+logging.basicConfig(level=logging.INFO)
+BASE_PATH = os.path.dirname(
+    os.path.abspath(__file__)
+)
+
 
 class InstallCommand(install):
     def run(self):
+        logger.info("Logger in setup.py")
         self.run_command('compile_mo_files')
+        if os.path.exists(
+            os.path.join(
+                BASE_PATH,
+                "src/climsoft_api/locale/fr/LC_MESSAGES/climsoft_messages.mo"
+            )
+        ):
+            raise FileExistsError(os.path.join(
+                BASE_PATH,
+                "src/climsoft_api/locale/fr/LC_MESSAGES/climsoft_messages.mo"
+            ))
         install.run(self)
 
 

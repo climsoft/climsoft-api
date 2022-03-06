3cea93c2298cf7877d033c20ec329b0e1ee493be
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-03-06 13:12:28 +0600

    add translation for instrument fault report schema

--- src/climsoft_api/api/instrumentfaultreport/schema.py
+++ src/climsoft_api/api/instrumentfaultreport/schema.py
@@ -8,14 +8,14 @@ from pydantic import constr, Field
 
 
 class CreateInstrumentFaultReport(BaseSchema):
-    refersTo: constr(max_length=255)
-    reportId: int
-    reportDatetime: constr(max_length=50)
-    faultDescription: constr(max_length=255)
-    reportedBy: constr(max_length=255)
-    receivedDatetime: constr(max_length=50)
-    receivedBy: constr(max_length=255)
-    reportedFrom: constr(max_length=255)
+    refersTo: constr(max_length=255) = Field(title=_("Refers To"))
+    reportId: int = Field(title=_("Report ID"))
+    reportDatetime: constr(max_length=50) = Field(title=_("Report Datetime"))
+    faultDescription: constr(max_length=255) = Field(title=_("Fault Description"))
+    reportedBy: constr(max_length=255) = Field(title=_("Reported By"))
+    receivedDatetime: constr(max_length=50) = Field(title=_("Received Datetime"))
+    receivedBy: constr(max_length=255) = Field(title=_("Received By"))
+    reportedFrom: constr(max_length=255) = Field(title=_("Reported From"))
 
     class Config:
         fields = {
@@ -31,13 +31,13 @@ class CreateInstrumentFaultReport(BaseSchema):
 
 
 class UpdateInstrumentFaultReport(BaseSchema):
-    refersTo: constr(max_length=255)
-    reportDatetime: constr(max_length=50)
-    faultDescription: constr(max_length=255)
-    reportedBy: constr(max_length=255)
-    receivedDatetime: constr(max_length=50)
-    receivedBy: constr(max_length=255)
-    reportedFrom: constr(max_length=255)
+    refersTo: constr(max_length=255) = Field(title=_("Refers To"))
+    reportDatetime: constr(max_length=50) = Field(title=_("Report Datetime"))
+    faultDescription: constr(max_length=255) = Field(title=_("Fault Description"))
+    reportedBy: constr(max_length=255) = Field(title=_("Reported By"))
+    receivedDatetime: constr(max_length=50) = Field(title=_("Received Datetime"))
+    receivedBy: constr(max_length=255) = Field(title=_("Received By"))
+    reportedFrom: constr(max_length=255) = Field(title=_("Reported From"))
 
     class Config:
         fields = {
@@ -53,8 +53,8 @@ class UpdateInstrumentFaultReport(BaseSchema):
 
 
 class InstrumentFaultReport(CreateInstrumentFaultReport):
-    reportDatetime: datetime.datetime
-    receivedDatetime: datetime.datetime
+    reportDatetime: datetime.datetime = Field(title=_("Report Datetime"))
+    receivedDatetime: datetime.datetime = Field(title=_("Received Datetime"))
 
     class Config:
         orm_mode = True
@@ -72,8 +72,8 @@ class InstrumentFaultReport(CreateInstrumentFaultReport):
 
 
 class InstrumentFaultReportWithStationAndInstrument(InstrumentFaultReport):
-    station: station_schema.Station
-    instrument: instrument_schema.Instrument
+    station: station_schema.Station = Field(title=_("Station"))
+    instrument: instrument_schema.Instrument = Field(title=_("Instrument"))
 
 
 class InstrumentFaultReportResponse(Response):
@@ -86,5 +86,5 @@ class InstrumentFaultReportWithStationAndInstrumentResponse(Response):
 
 class InstrumentFaultReportQueryResponse(InstrumentFaultReportResponse):
     limit: int = Field(title=_("Limit"))
-    page: int
-    pages: int
+    page: int = Field(title=_("Page"))
+    pages: int = Field(title=_("Pages"))

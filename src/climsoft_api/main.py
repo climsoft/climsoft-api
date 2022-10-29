1a49526896d14cf3f93e05eeec0991cb4327a6f0
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-10-29 14:22:39 +0200

    put none if required

--- src/climsoft_api/main.py
+++ src/climsoft_api/main.py
@@ -39,7 +39,7 @@ def get_app(config=None):
     async def db_session_middleware(request: Request, call_next):
         try:
             request.state.get_session = get_session_local(config)
-            request.state.settings_override = deployment_configs[config]
+            request.state.settings_override = deployment_configs.get(config)
             response = await call_next(request)
         except Exception as exc:
             logging.exception(exc)
